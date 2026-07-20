393a43214a04ca7be87bc16e6463d7fee354a955
Sirojiddin Uljayev <uljayevsirojiddin570@gmail.com>
2026-07-20 22:12:44 +0500

    Day 9: Return intro

---
+++ 01_Python/day9_return_intro.py
@@ -0,0 +1,4 @@
+def engine_power():
+    return(635)
+power = engine_power()
+print("Engine power: ", power, "HP")
---
+++ 01_Python/return_intro_second.py
@@ -0,0 +1,4 @@
+def add(a, b):
+    return(a+b)
+result = add(540, 95)
+print(result)
